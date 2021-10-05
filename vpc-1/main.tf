aa1f9992821a0f37be9adbcd6105037411778c6a
Perugu swathi <Peruguswathi11@gmail.com>
2021-10-05 16:49:19 +0530

    @123

---
+++ vpc-1/main.tf
@@ -0,0 +1,114 @@
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+/*create vpc*/
+resource "aws_vpc" "vpc" {
+  cidr_block              = var.vpc-cidr
+  instance_tenancy        = "default"
+  enable_dns_hostnames    = true
+
+  tags      = {
+    Name    = "vpc1"
+  }
+}
+
+/*creating internet_gateway*/
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "IGW"
+  }
+}
+
+/*creating public subnets*/
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public-subnet-1-cidr
+  availability_zone       = "us-east-1a"
+  
+  tags      = {
+    Name    = "Public Subnet 1"
+  }
+}
+
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public-subnet-2-cidr
+  availability_zone       = "us-east-1b"
+  
+  tags      = {
+    Name    = "Public Subnet 2"
+  }
+}
+
+resource "aws_subnet" "public-subnet-3" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public-subnet-3-cidr
+  availability_zone       = "us-east-1c"
+
+  tags      = {
+    Name    = "Public Subnet 3"
+  }
+}
+
+/*creating public routetable*/
+resource "aws_route_table" "public-route-table" {
+  vpc_id       = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags       = {
+    Name     = "Public Route Table"
+  }
+}
+
+/*Associate Public Subnets to routetables*/
+resource "aws_route_table_association" "public-subnet-1-route-table-association" {
+  subnet_id           = aws_subnet.public-subnet-1.id
+  route_table_id      = aws_route_table.public-route-table.id
+}
+resource "aws_route_table_association" "public-subnet-3-route-table-association" {
+  subnet_id           = aws_subnet.public-subnet-2.id
+  route_table_id      = aws_route_table.public-route-table.id
+}
+resource "aws_route_table_association" "public-subnet-2-route-table-association" {
+  subnet_id           = aws_subnet.public-subnet-3.id
+  route_table_id      = aws_route_table.public-route-table.id
+}
+
+/*creating private subnets*/
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private-subnet-1-cidr
+  availability_zone       = "us-east-1a"
+ 
+  tags      = {
+    Name    = "private Subnet 1"
+  }
+}
+
+resource "aws_subnet" "private-subnet-2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private-subnet-2-cidr
+  availability_zone       = "us-east-1b"
+  
+  tags      = {
+    Name    = "private Subnet 2"
+  }
+}
+
+resource "aws_subnet" "private-subnet-3" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private-subnet-3-cidr
+  availability_zone       = "us-east-1c"
+ 
+  tags      = {
+    Name    = "private Subnet 3"
+  }
+}
