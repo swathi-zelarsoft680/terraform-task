8e9584e38fa871f750ff231f3a5570bf462a43da
swathi-zelarsoft680 <swathi.p@zelarsoft.com>
2021-10-06 16:30:47 +0530

    @added bucket

---
+++ s3bucket/main.tf
@@ -0,0 +1,33 @@
+#  terraform {
+#   backend "s3" {
+#     bucket = "binnubucket"
+#     key    = "terraform.tfstate"
+#     region = "us-east-1"
+#     profile = "default"
+#     shared_credentials_file = "D:/.aws/credentials"
+#   }
+
+#    required_providers {
+#     aws = {
+#        source  = "hashicorp/aws"
+#        version = "~> 3.27"
+#      }
+#    }
+
+#    required_version = ">= 0.14.9"
+#  }
+
+ provider "aws" {
+  profile = "default"
+  region  = var.region
+#  shared_credentials_file = "D:/.aws/credentials"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "binnubucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "binnubucket"
+  }
+}
