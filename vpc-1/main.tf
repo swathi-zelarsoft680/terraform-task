97d6bc596a53f98d4df39d1a11d93b6176f2d497
swathi-zelarsoft680 <swathi.p@zelarsoft.com>
2021-10-06 15:15:57 +0530

    #added

--- vpc-1/main.tf
+++ vpc-1/main.tf
@@ -1,20 +1,20 @@
-terraform {
-  backend "remote" {
-    organization = "swathizs"
+ terraform {
+   backend "remote" {
+     organization = "swathizs"
 
-    workspaces {
-      name = "swathi"
-    }
-  }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
+     workspaces {
+       name = "swathi"
+     }
+   }
+   required_providers {
+     aws = {
+       source  = "hashicorp/aws"
       version = "~> 3.27"
     }
-  }
+   }
 
-  required_version = ">= 1.0.6"
-}
+   required_version = ">= 1.0.6"
+ }
 provider "aws" {
   profile = "default"
   region  = var.region
@@ -41,38 +41,38 @@ resource "aws_internet_gateway" "gw" {
 }
 
 /*creating public subnets*/
-resource "aws_subnet" "public-subnet-1" {
+resource "aws_subnet" "pub-subnet-1" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public-subnet-1-cidr
+  cidr_block              = var.pub-subnet-1-cidr
   availability_zone       = "us-east-1a"
   
   tags      = {
-    Name    = "Public Subnet 1"
+    Name    = "Pub Subnet 1"
   }
 }
 
-resource "aws_subnet" "public-subnet-2" {
+resource "aws_subnet" "pub-subnet-2" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public-subnet-2-cidr
+  cidr_block              = var.pub-subnet-2-cidr
   availability_zone       = "us-east-1b"
   
   tags      = {
-    Name    = "Public Subnet 2"
+    Name    = "Pub Subnet 2"
   }
 }
 
-resource "aws_subnet" "public-subnet-3" {
+resource "aws_subnet" "pub-subnet-3" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public-subnet-3-cidr
+  cidr_block              = var.pub-subnet-3-cidr
   availability_zone       = "us-east-1c"
 
   tags      = {
-    Name    = "Public Subnet 3"
+    Name    = "Pub Subnet 3"
   }
 }
 
 /*creating public routetable*/
-resource "aws_route_table" "public-route-table" {
+resource "aws_route_table" "pub-route-table" {
   vpc_id       = aws_vpc.vpc.id
 
   route {
@@ -81,51 +81,51 @@ resource "aws_route_table" "public-route-table" {
   }
 
   tags       = {
-    Name     = "Public Route Table"
+    Name     = "Pub Route Table"
   }
 }
 
 /*Associate Public Subnets to routetables*/
-resource "aws_route_table_association" "public-subnet-1-route-table-association" {
-  subnet_id           = aws_subnet.public-subnet-1.id
-  route_table_id      = aws_route_table.public-route-table.id
+resource "aws_route_table_association" "pub-subnet-1-route-table-association" {
+  subnet_id           = aws_subnet.pub-subnet-1.id
+  route_table_id      = aws_route_table.pub-route-table.id
 }
-resource "aws_route_table_association" "public-subnet-3-route-table-association" {
-  subnet_id           = aws_subnet.public-subnet-2.id
-  route_table_id      = aws_route_table.public-route-table.id
+resource "aws_route_table_association" "pub-subnet-3-route-table-association" {
+  subnet_id           = aws_subnet.pub-subnet-2.id
+  route_table_id      = aws_route_table.pub-route-table.id
 }
-resource "aws_route_table_association" "public-subnet-2-route-table-association" {
-  subnet_id           = aws_subnet.public-subnet-3.id
-  route_table_id      = aws_route_table.public-route-table.id
+resource "aws_route_table_association" "pub-subnet-2-route-table-association" {
+  subnet_id           = aws_subnet.pub-subnet-3.id
+  route_table_id      = aws_route_table.pub-route-table.id
 }
 
 /*creating private subnets*/
-resource "aws_subnet" "private-subnet-1" {
+resource "aws_subnet" "pri-subnet-1" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private-subnet-1-cidr
+  cidr_block              = var.pri-subnet-1-cidr
   availability_zone       = "us-east-1a"
  
   tags      = {
-    Name    = "private Subnet 1"
+    Name    = "pri Subnet 1"
   }
 }
 
-resource "aws_subnet" "private-subnet-2" {
+resource "aws_subnet" "pri-subnet-2" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private-subnet-2-cidr
+  cidr_block              = var.pri-subnet-2-cidr
   availability_zone       = "us-east-1b"
   
   tags      = {
-    Name    = "private Subnet 2"
+    Name    = "pri Subnet 2"
   }
 }
 
-resource "aws_subnet" "private-subnet-3" {
+resource "aws_subnet" "pri-subnet-3" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private-subnet-3-cidr
+  cidr_block              = var.pri-subnet-3-cidr
   availability_zone       = "us-east-1c"
  
   tags      = {
-    Name    = "private Subnet 3"
+    Name    = "pri Subnet 3"
   }
 }
---
+++ vpc-1/variables.tf
@@ -0,0 +1,49 @@
+variable "region" {
+  default       = "us-east-1"
+  description   = "AWS Region"
+  type          = string
+}
+
+variable "vpc-cidr" {
+  default       = "10.0.0.0/16"
+  description   = "VPC CIDR Block"
+  type          = string
+}
+
+variable "pub-subnet-1-cidr" {
+  default       = "10.0.0.0/24"
+  description   = "Public Subnet 1 CIDR Block"
+  type          = string
+}
+
+variable "pub-subnet-2-cidr" {
+  default       = "10.0.1.0/24"
+  description   = "Public Subnet 2 CIDR Block"
+  type          = string
+}
+
+variable "pub-subnet-3-cidr" {
+  default       = "10.0.2.0/24"
+  description   = "Public Subnet 3 CIDR Block"
+  type          = string
+}
+
+variable "pri-subnet-1-cidr" {
+  default       = "10.0.3.0/24"
+  description   = "Private Subnet 1 CIDR Block"
+  type          = string
+}
+
+variable "pri-subnet-2-cidr" {
+  default       = "10.0.4.0/24"
+  description   = "Private Subnet 2 CIDR Block"
+  type          = string
+}
+
+variable "pri-subnet-3-cidr" {
+  default       = "10.0.5.0/24"
+  description   = "Private Subnet 3 CIDR Block"
+  type          = string
+}
+
+
