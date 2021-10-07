1658cb537b5731fe70de01b5ca881ea996b5a35c
swathi-zelarsoft680 <swathi.p@zelarsoft.com>
2021-10-07 18:38:53 +0530

    @123

---
+++ GCP/vpc/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("C:/Users/ragsr/Downloads/zelarsoft-99636fd402c8.json")
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
