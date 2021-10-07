f40180d9311bbe98ecea3082742bb51122cd4b43
swathi-zelarsoft680 <swathi.p@zelarsoft.com>
2021-10-07 17:14:15 +0530

    @foreach

---
+++ for-each/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-1"
+}
+variable "vpcs" {
+  type = map(object({
+    cider = string
+    tags = map(string)
+    tenancy =string
+  }))
+ default = {
+   "one" = {
+     cidr = "10.0.0.0.16"
+     tags = {
+       "Name" = "vpc-one"  
+     } 
+     tenancy = "default"
+   } 
+   "two" = {
+     cidr = "10.20.0.0.16"
+     tags = {
+       "Name" = "vpc-two"  
+     } 
+     tenancy = "default"
+   }  
+ }
+}
+
+resource "aws_vpc" "main" {
+  for_each = var.vpcs
+  cidr_block = each.value["cidr"]
+  instance_tenency = each.value["tenancy"]
+  tags = each.value["tags"]
+}
