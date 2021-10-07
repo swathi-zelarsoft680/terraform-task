1ba9e6789cbd2c725dea54a16b3456d4dfd3cbb2
swathi-zelarsoft680 <swathi.p@zelarsoft.com>
2021-10-07 17:25:55 +0530

    @new

--- for-each/main.tf
+++ for-each/main.tf
@@ -11,14 +11,14 @@ variable "vpcs" {
   }))
  default = {
    "one" = {
-     cidr = "10.0.0.0.16"
+     cidr = "10.0.0.0/16"
      tags = {
        "Name" = "vpc-one"  
      } 
      tenancy = "default"
    } 
    "two" = {
-     cidr = "10.20.0.0.16"
+     cidr = "10.20.0.0/16"
      tags = {
        "Name" = "vpc-two"  
      } 
