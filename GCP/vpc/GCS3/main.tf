29cea0dd5f470e487b29077632ec4bbe3bd1e620
swathi-zelarsoft680 <swathi.p@zelarsoft.com>
2021-10-08 11:06:36 +0530

    @123

---
+++ GCP/vpc/GCS3/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("C:/Users/ragsr/Downloads/zelarsoft-99636fd402c8.json")
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_storage_bucket" "GCP" {
+  name          = "swathi"
+  location      = "EU"
+  force_destroy = true
+
+}
