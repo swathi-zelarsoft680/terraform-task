07eb381e0787bed47c1eba0d86285ba50cfbec14
swathi-zelarsoft680 <swathi.p@zelarsoft.com>
2021-10-08 11:18:59 +0530

    cd

---
+++ GCP/vpc/VMs/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("C:/Users/ragsr/Downloads/zelarsoft-99636fd402c8.json")
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
